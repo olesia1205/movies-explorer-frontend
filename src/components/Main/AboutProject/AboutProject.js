9ff6c860eaf44bcb0cfa0ddfb77dd1447d445eda
Olesia Erastova <olesia1205@yandex.ru>
2023-05-15 10:55:11 +0300

    верстка компонента AboutProject

---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function AboutProject() {
+  return (
+    <div className="aboutProject__container" id="aboutProject">
+      <h2 className="aboutProject__subtitle">О проекте</h2>
+      <div className="aboutProject__infoblock">
+        {/* секция с этапами диплома */}
+        <section className="aboutProject__section" aria-label="Описание этапов диплома">
+          <h3 className="aboutProject__section-subtitle">Дипломный проект включал 5 этапов</h3>
+          <p className="aboutProject__section-text">Составление плана, работу над бэкендом, вёрстку,
+          добавление функциональности и финальные доработки.</p>
+          <p className="aboutProject__text_timing">1 неделя</p>
+          <p className="aboutProject__text_timing aboutProject__text">Back-end</p>
+        </section>
+
+        {/* секция со сроками выполнения диплома */}
+        <section className="aboutProject__section" aria-label="Описание сроков выполнения диплома">
+          <h3 className="aboutProject__section-subtitle">На выполнение диплома ушло 5 недель</h3>
+          <p className="aboutProject__section-text">У каждого этапа был мягкий и жёсткий дедлайн,
+          которые нужно было соблюдать, чтобы успешно защититься.</p>
+          <p className="aboutProject__text_timing aboutProject__text_timing-white">4 недели</p>
+          <p className="aboutProject__text_timing aboutProject__text aboutProject__text_removed">Front-end</p>
+        </section>
+      </div>
+
+    </div>
+  );
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Promo from '../Main/Promo/Promo';
 import NavTab from '../Main/NavTab/NavTab';
+import AboutProject from '../Main/AboutProject/AboutProject';
 
 function Main() {
   return (
     <main>
       <Promo />
       <NavTab />
+      <AboutProject />
     </main>
   );
 }
