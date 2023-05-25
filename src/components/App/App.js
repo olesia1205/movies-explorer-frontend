90e94ebc9de9d14d83ec33f9f21204a8ea8a2b5e
Olesia Erastova <olesia1205@yandex.ru>
2023-05-25 09:52:12 +0300

    верстка Login Register PageNotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,10 @@ import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Register from '../Register/Register';
 import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import PageNotFound from '../PageNotFound/PageNotFound';
 import moviesArray from '../../constants/moviesArray';
 import { savedMoviesArray } from '../../constants/savedMoviesArray';
 
@@ -70,6 +73,21 @@ function App() {
               />
               <Profile />
             </>
+          }
+        />
+        <Route path='/signup'
+          element={
+            <Register />
+          }
+        />
+        <Route path='/signin'
+          element={
+            <Login />
+          }
+        />
+        <Route path='*'
+          element={
+            <PageNotFound/>
           }
         />
       </Routes>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,11 +7,11 @@ function Profile() {
       <fieldset className="profile__inputs-block">
         <label className="profile__label">
           <p className="profile__placeholder">Имя</p>
-          <input className="profile__input profile__input-name" type="text" value="Виталий" />
+          <input className="profile__input profile__input-name" type="text" placeholder="Виталий" required minLength="2"/>
         </label>
         <label className="profile__label">
           <p className="profile__placeholder">E-mail</p>
-          <input className="profile__input profile__input-email" type="email" value="pochta@yandex.ru" />
+          <input className="profile__input profile__input-email" type="email" placeholder="pochta@yandex.ru" required minLength="2"/>
         </label>
       </fieldset>
       <div className="profile__buttons-block">
