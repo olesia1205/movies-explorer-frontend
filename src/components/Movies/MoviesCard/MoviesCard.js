5b95d9ed8a910c2bb345fdbe116e898a09b98791
Olesia Erastova <olesia1205@yandex.ru>
2023-06-04 20:27:16 +0300

    добавила функцию конвертации продолжительности фильмов

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import likeButton from '../../../images/like-button_active.svg';
 // import dislikeButton from '../../../images/like-button_inactive.svg';
+import movieDuration from '../../../utils/DurationChange';
 
 function MoviesCard({ country, director, duration, year, description, image, trailerLink, thumbnail, movieId, nameRU, nameEN }) {
+  const convertedDuration = movieDuration(duration);
   return (
     <article className="movie">
       <img className="movie__image" src={image} alt={nameRU} />
       <div className="movie__info">
         <figcaption className="movie__figcaption">
           <h2 className="movie__title">{nameRU}</h2>
-          <h3 className="movie__duration">{duration}</h3>
+          <h3 className="movie__duration">{convertedDuration}</h3>
         </figcaption>
         <button className="movie__like-button" name="movie__like-button" type="button">
           <img className="movie__like-image" src={likeButton} alt="Кнопка лайка"></img>
